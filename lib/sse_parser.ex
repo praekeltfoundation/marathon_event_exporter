679e2dea1f77f897a1e09872ebf89103454e8c53
Jeremy Thurgood <jeremy@praekelt.org>
2017-11-29 17:57:21 +0200

    The rest of the SSE parser.

--- lib/sse_parser.ex
+++ lib/sse_parser.ex
@@ -14,24 +14,36 @@ defmodule MarathonEventExporter.SSEParser do
   end
 
   defmodule State do
-    defstruct listeners: MapSet.new, event: %Event{}, line_part: ""
+    defstruct listeners: MapSet.new(), event: %Event{}, line_part: ""
   end
 
 
   ## Client API
 
+  @doc """
+  Starts a new SSEParser.
+  """
   def start_link(opts) do
     GenServer.start_link(__MODULE__, :ok, opts)
   end
 
+  @doc """
+  Feeds some input data into the parser.
+  """
   def feed_data(server, data) do
     GenServer.cast(server, {:feed_data, data})
   end
 
+  @doc """
+  Registers a listener process to receive parsed events.
+  """
   def register_listener(server, pid) do
     GenServer.call(server, {:register_listener, pid})
   end
 
+  @doc """
+  Unregisters a listener process.
+  """
   def unregister_listener(server, pid) do
     GenServer.call(server, {:unregister_listener, pid})
   end
@@ -65,9 +77,8 @@ defmodule MarathonEventExporter.SSEParser do
 
   ## Internals
 
-  def emit_event(state) do
-    IO.puts("event: #{inspect state.event}")
-    %{state | event: %Event{}}
+  def emit_event(event, state) do
+    Enum.each(state.listeners, fn l -> send(l, {:sse, event}) end)
   end
 
 
@@ -90,13 +101,51 @@ defmodule MarathonEventExporter.SSEParser do
   end
 
   defp line_complete(data, state) do
-    new_state = line_received(state.line, %{state | line_part: ""})
+    new_state = line_received(state.line_part, %{state | line_part: ""})
     data_received(data, new_state)
   end
 
-  defp line_received(line, state) do
-    IO.puts("line: #{line}")
-    state
+  # Handle an empty line, which indicates the end of an event.
+  defp line_received("", state) do
+    if state.event.data != "" do
+      # Remove one trailing newline (if there is one).
+      data = String.replace_suffix(state.event.data, "\n", "")
+      emit_event(%{state.event | data: data}, state)
+    end
+    %{state | event: %Event{}}
   end
+  # Handle a comment by ignoring it.
+  defp line_received(":" <> _, state), do: state
+  # Parse the line into the field and value for further processing.
+  defp line_received(line, state) do
+    {field, value} = case String.split(line, ":") do
+                       [field] -> {field, ""}
+                       [field, " " <> value] -> {field, value}
+                       [field, value] -> {field, value}
+                     end
+    process_field(field, value, state)
+  end
+
+  # Append the data value to the data field with a trailing newline.
+  defp process_field("data", value, state) do
+    new_event = %{state.event | data: state.event.data <> value <> "\n"}
+    %{state | event: new_event}
+  end
+  # Set the event field to the event value.
+  defp process_field("event", value, state) do
+    new_event = %{state.event | event: value}
+    %{state | event: new_event}
+  end
+  # Set the id field to the id value if the value does not contain a NUL.
+  defp process_field("id", value, state) do
+    cond do
+      String.contains?(value, <<0>>) -> state.event
+      true ->
+        new_event = %{state.event | id: value}
+        %{state | event: new_event}
+    end
+  end
+  # Ignore any other field.
+  defp process_field(_field, _value, state), do: state
 
 end
--- test/sse_parser_test.exs
+++ test/sse_parser_test.exs
@@ -4,8 +4,18 @@ defmodule MarathonEventExporter.SSEParserTest do
   alias MarathonEventExporter.SSEParser
   alias SSEParser.{Event, State}
 
+  defmodule EventCatcher do
+    use GenServer
+
+    def start_link(opts), do: GenServer.start_link(__MODULE__, [], opts)
+    def events(server), do: GenServer.call(server, :events)
+
+    def handle_call(:events, _from, events), do: {:reply, events, events}
+    def handle_info({:sse, event}, events), do: {:noreply, [event | events]}
+  end
+
   setup do
-    {:ok, ssep} = start_supervised SSEParser
+    {:ok, ssep} = start_supervised(SSEParser)
     %{ssep: ssep}
   end
 
@@ -29,6 +39,26 @@ defmodule MarathonEventExporter.SSEParserTest do
     assert get_state(ssep) == %State{listeners: MapSet.new()}
     assert SSEParser.unregister_listener(ssep, self()) == :ok
     assert get_state(ssep) == %State{listeners: MapSet.new()}
+  end
+
+  test "a listener receives events", %{ssep: ssep} do
+    {:ok, listener} = start_supervised(EventCatcher)
+    assert SSEParser.register_listener(ssep, listener) == :ok
+    assert EventCatcher.events(listener) == []
+    assert SSEParser.feed_data(ssep, "data: hello\n\n") == :ok
+    assert get_state(ssep).event == %Event{}
+    assert EventCatcher.events(listener) == [%Event{data: "hello"}]
+  end
+
+  test "multiple listeners receive events", %{ssep: ssep} do
+    {:ok, l1} = start_supervised(EventCatcher, id: :l1)
+    {:ok, l2} = start_supervised(EventCatcher, id: :l2)
+    assert SSEParser.register_listener(ssep, l1) == :ok
+    assert SSEParser.register_listener(ssep, l2) == :ok
+    assert SSEParser.feed_data(ssep, "data: sanibonani\n\n") == :ok
+    assert get_state(ssep).event == %Event{}
+    assert EventCatcher.events(l1) == [%Event{data: "sanibonani"}]
+    assert EventCatcher.events(l2) == [%Event{data: "sanibonani"}]
   end
 
   test "feed_data", %{ssep: ssep} do
