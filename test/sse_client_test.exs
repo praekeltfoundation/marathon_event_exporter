c12608f83df8079517a42c0b77303768b1d907b0
Jamie Hewland <jhewland@gmail.com>
2017-12-22 12:38:53 +0200

    Make the MarathonClient just an SSEClient

--- test/sse_client_test.exs
+++ test/sse_client_test.exs
@@ -1,14 +1,18 @@
-defmodule MarathonEventExporter.MarathonClientTest do
+defmodule MarathonEventExporter.SSEClientTest do
   use ExUnit.Case, async: true
 
-  alias MarathonEventExporter.MarathonClient
+  alias MarathonEventExporter.SSEClient
   import TestHelpers
   import ExUnit.CaptureLog
 
-  test "stream_events streams events to a listener process" do
+  def stream_events(fm, timeout \\ 60_000) do
+    url = FakeMarathon.base_url(fm) <> "/v2/events"
+    SSEClient.start({url, [self()], timeout})
+  end
+
+  test "the SSE client streams events to a listener process" do
     {:ok, fm} = start_supervised(FakeMarathon)
-    base_url = FakeMarathon.base_url(fm)
-    {:ok, _} = MarathonClient.stream_events(base_url, [self()])
+    {:ok, _} = stream_events(fm)
 
     # Stream an event, assert that we receive it within a second.
     event = marathon_event("event_stream_attached", remoteAddress: "127.0.0.1")
@@ -21,10 +25,9 @@ defmodule MarathonEventExporter.MarathonClientTest do
     assert_receive {:sse, ^event2}, 1_000
   end
 
-  test "stream_events exits when the server connection is closed" do
+  test "the SSE client exits when the server connection is closed" do
     {:ok, fm} = start_supervised(FakeMarathon)
-    base_url = FakeMarathon.base_url(fm)
-    {:ok, se} = MarathonClient.stream_events(base_url, [self()])
+    {:ok, se} = stream_events(fm)
     ref = Process.monitor(se)
 
     # Stream an event, assert that we receive it within a second.
@@ -37,25 +40,24 @@ defmodule MarathonEventExporter.MarathonClientTest do
     assert_receive {:DOWN, ^ref, :process, _, :normal}, 1_000
   end
 
-  test "stream_events fails on a bad response" do
+  test "the SSE client fails on a bad response" do
     # Trap exits so the start_link in stream_events doesn't break the test.
     Process.flag(:trap_exit, true)
     {:ok, fm} = start_supervised(FakeMarathon)
     base_url = FakeMarathon.base_url(fm)
-    {:error, err} =  MarathonClient.stream_events(base_url <> "/bad", [self()])
+    {:error, err} =  SSEClient.start({base_url <> "/bad", [self()], 60_000})
     assert err =~ ~r/Error connecting to event stream: .*{code: 404/
   end
 
-  test "stream_events only returns once a response is received" do
+  test "the SSE client only starts once a response is received" do
     # On my machine, without waiting for the response, the delay is
     # consistently under 100ms. I chose 250ms here as a balance between
     # incorrect results and waiting too long.
     delay_ms = 250
 
     {:ok, fm} = start_supervised({FakeMarathon, [response_delay: delay_ms]})
-    base_url = FakeMarathon.base_url(fm)
     t0 = Time.utc_now()
-    {:ok, _} = MarathonClient.stream_events(base_url, [self()])
+    {:ok, _} = stream_events(fm)
     t1 = Time.utc_now()
     assert Time.diff(t1, t0, :milliseconds) >= delay_ms
 
@@ -65,11 +67,10 @@ defmodule MarathonEventExporter.MarathonClientTest do
     assert_receive {:sse, ^event}, 1_000
   end
 
-  test "stream_events times out if no data is received for too long" do
+  test "the SSE client times out if no data is received for too long" do
     {:ok, fm} = start_supervised(FakeMarathon)
-    base_url = FakeMarathon.base_url(fm)
-    {:ok, se} = MarathonClient.stream_events(base_url, [self()], 100)
-    ref = Process.monitor(se)
+    {:ok, sc} = stream_events(fm, 100)
+    ref = Process.monitor(sc)
 
     # Send keepalives for longer than our timeout interval.
     FakeMarathon.keepalive(fm)
